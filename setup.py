86b39a206bf89cc04415b6dae13bbff5df4256d3
alexberazouski <alex.berazouski@gmail.com>
2020-10-24 13:01:25 +0200

    Initial implementation

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('README.md') as f:
+    readme = f.read()
+
+# print(find_packages(exclude=('examples')))
+# print(license)
+
+setup(
+    name='degiropy',
+    version='0.1.1',
+    description='Unofficial Degiro API in python',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Alex Berazouski',
+    author_email='alex.berazouski@gmail.com',
+    license=license,
+    packages=find_packages(exclude=('examples')),
+    install_requires=['requests'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD 2 License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    zip_safe=False
+)
