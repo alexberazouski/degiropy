8f23571037829e48b289ec5ffc4e9b67ccff89cb
Alex Berazouski <alex.berazouski@gmail.com>
2020-10-24 18:23:19 +0200

    Add usage examples
    fixed long_description

--- setup.py
+++ setup.py
@@ -1,28 +1,23 @@
 from setuptools import setup, find_packages
 
-with open('LICENSE') as f:
-    license = f.read()
-
 with open('README.md') as f:
     readme = f.read()
 
-# print(find_packages(exclude=('examples')))
-# print(license)
-
 setup(
     name='degiropy',
-    version='0.1.1',
+    version='0.1.2',
     description='Unofficial Degiro API in python',
     long_description=readme,
     long_description_content_type='text/markdown',
+    url='https://github.com/alexberazouski/degiropy',
     author='Alex Berazouski',
     author_email='alex.berazouski@gmail.com',
-    license=license,
+    license='BSD 2',
     packages=find_packages(exclude=('examples')),
     install_requires=['requests'],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: BSD 2 License',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
